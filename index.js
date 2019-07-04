23e77ad3d2566f94cebeeb540d9e275329293f5e
rikumi <riku.mi@outlook.com>
2019-07-04 04:02:17 +0000

    Session storage, batch send, native replies & emoji bugfix

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Twitter = require('twitter');
 const Telegraf = require('telegraf');
 const { OAuth } = require('oauth');
 const Koa = require('koa');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
 
 const config = require('./config.json');
 
@@ -12,19 +15,55 @@ const oa = new OAuth(
     config.twitterConsumerKey,
     config.twitterConsumerSecret,
     '1.0A',
-    'http://34.80.161.163/',
+    `http://${ config.serverIp }/`,
     'HMAC-SHA1'
 );
 const srv = new Koa();
-const session = {};    // key    to session
-const sessionMap = {}; // chatId to session
 const msgOptions = {
     parse_mode: 'HTML',
     disable_web_page_preview: true
 };
+const rl = readline.createInterface(process.stdin);
+
+const secretTemp = {}; // { [requestToken]: { chatId, secret: requestSecret } }
+const sessions = {};   // { [chatId]: { token, secret, client, lastId, task, ... } }
+const sessionStorePath = path.join(__dirname, 'session.json');
+
+if (fs.existsSync(sessionStorePath)) {
+    try {
+        Object.assign(sessions, JSON.parse(fs.readFileSync(sessionStorePath)));
+
+        for (let chatId in sessions) {
+            let session = sessions[chatId];
+
+            session.client = new Twitter({
+                consumer_key: config.twitterConsumerKey,
+                consumer_secret: config.twitterConsumerSecret,
+                access_token_key: session.token,
+                access_token_secret: session.secret
+            });
+            session.task = setInterval(update.bind(null, chatId), 60000);
+            process.nextTick(update.bind(null, chatId));
+        }
+        console.log(`Loaded ${ Object.keys(sessions).length } session(s).`)
+    } catch (e) {
+        console.error('Failed to load json sessions:', e)
+    }
+}
 
 process.on('uncaughtException', console.error);
 process.on('unhandledRejection', (e) => { throw e });
+process.on('SIGINT', () => {
+    if (Object.keys(sessions).length) {
+        for (let key in sessions) {
+            delete sessions[key].client;
+            delete sessions[key].task;
+        }
+        fs.writeFileSync(sessionStorePath, JSON.stringify(sessions, null, 4));
+        console.log(`Saved ${ Object.keys(sessions).length } session(s). Quitting...`);
+    }
+    process.exit(0);
+});
 
 bot.start((ctx) => {
     oa.getOAuthRequestToken((error, key, secret) => {
@@ -32,86 +71,119 @@ bot.start((ctx) => {
             console.log(error);
             ctx.reply('Error fetching OAuth Token.');
         } else {
-            let chatId = ctx.message.chat.id;
-            sessionMap[chatId] = session[key] = { secret, chat: chatId };
+            let { username } = ctx.message.from;
+            secretTemp[key] = {
+                secret,
+                chatId: ctx.message.chat.id,
+                username
+            };
             ctx.reply(`<a href="https://api.twitter.com/oauth/authenticate?oauth_token=${ key }">Sign in with Twitter</a>`, msgOptions);
+            console.log(`New user: @${ username } is authenticating...`);
         }
     });
 });
 
 srv.use(async (ctx, next) => {
-    let { oauth_token: key, oauth_verifier: code } = ctx.query;
-    if (!key || !code) ctx.throw(400);
+    let { oauth_token: tempToken, oauth_verifier: code } = ctx.query;
+    if (!tempToken || !code) ctx.throw(400);
 
     await new Promise((resolve) => {
-        oa.getOAuthAccessToken(key, session[key].secret, code, (error, token, secret) => {
+        let { chatId, secret: tempSecret, username } = secretTemp[tempToken];
+        delete secretTemp[tempToken];
+        
+        oa.getOAuthAccessToken(tempToken, tempSecret, code, (error, token, secret) => {
             if (error) {
                 console.log(error);
                 ctx.body = 'Error fetching OAuth Token.';
             } else {
-                session[key].token = token;
-                session[key].secret = secret;
-                session[key].client = new Twitter({
-                    consumer_key: config.twitterConsumerKey,
-                    consumer_secret: config.twitterConsumerSecret,
-                    access_token_key: token,
-                    access_token_secret: secret
-                })
-                session[key].lastId = null;
-                session[key].task = setInterval(update.bind(null, key), 60000);
-                update(key);
+                sessions[chatId] = {
+                    username,
+                    token, secret,
+                    client: new Twitter({
+                        consumer_key: config.twitterConsumerKey,
+                        consumer_secret: config.twitterConsumerSecret,
+                        access_token_key: token,
+                        access_token_secret: secret
+                    }),
+                    lastId: null,
+                    task: setInterval(update.bind(null, chatId), 60000),
+                    recent: {}
+                }
+                process.nextTick(update.bind(null, chatId));
 
                 ctx.redirect('https://t.me/rikumi_bot');
+                console.log(`New user: @${ username } is signed in!`);
                 resolve();
             }
-        })
-    })
+        });
+    });
 });
 
-function update(key) {
-    with (session[key]) {
-        const options = {};
-        if (!lastId) {
-            options.count = 10;
-        } else {
-            options.since_id = lastId;
-        }
-        client.get('statuses/home_timeline', options, async (error, tweets) => {
-            if (error) {
-                console.log(error);
-                // bot.telegram.sendMessage(session[key].chat, 'Error fetching tweets.');
-            } else {
-                tweets = tweets.sort((a, b) => new Date(a.created_at) - new Date(b.created_at))
-                for (let tweet of tweets) {
-                    await sendTweet(session[key].chat, tweet);
-                    lastId = tweet.id_str;
-                }
-            }
-        })
+function update(chatId) {
+    let session = sessions[chatId];
+    const options = {};
+    if (!session.lastId) {
+        options.count = 10;
+    } else {
+        options.since_id = session.lastId;
     }
+    console.log(`Updating tweets for @${ session.username }.`);
+    
+    session.client.get('statuses/home_timeline', options, async (error, tweets) => {
+        if (error) {
+            console.log(`Error from @${ session.username }:`, error);
+        } else {
+            tweets = tweets.sort((a, b) => new Date(a.created_at) - new Date(b.created_at))
+            for (let tweet of tweets) {
+                await sendTweet(chatId, tweet);
+                session.lastId = tweet.id_str;
+            }
+            console.log(`Updated ${ tweets.length } tweet(s) for @${ session.username }.`);
+        }
+    });
 }
 
 async function sendTweet(chatId, tweet) {
+    let session = sessions[chatId];
     let { mediaType, mediaUrls, text } = buildTweet(tweet);
+    let result;
+
+    let options = { ...msgOptions };
+
+    let replied = tweet.in_reply_to_status_id_str;
+    if (replied && session.recent[replied]) {
+        options.reply_to_message_id = session.recent[replied];
+    }
 
     switch (mediaType) {
         case 'photo':
             if (mediaUrls.length > 1) {
                 let [pic] = await bot.telegram.sendMediaGroup(chatId, mediaUrls.map(url => ({ type: 'photo', media: url })));
-                await bot.telegram.sendMessage(chatId, text, { reply_to_message_id: pic.message_id, ...msgOptions });
+                options.reply_to_message_id = pic.message_id;
+                result = await bot.telegram.sendMessage(chatId, text, options);
             } else {
-                await bot.telegram.sendPhoto(chatId, mediaUrls[0], { caption: text, ...msgOptions });
+                options.caption = text;
+                result = await bot.telegram.sendPhoto(chatId, mediaUrls[0], options);
             }
             break;
         case 'video':
-            await bot.telegram.sendVideo(chatId, mediaUrls[0], { caption: text, ...msgOptions });
+            options.caption = text;
+            result = await bot.telegram.sendVideo(chatId, mediaUrls[0], options);
             break;
         case 'animated_gif':
-            await bot.telegram.sendAnimation(chatId, mediaUrls[0], { caption: text, ...msgOptions });
+            options.caption = text;
+            result = await bot.telegram.sendAnimation(chatId, mediaUrls[0], options);
             break;
         default:
-            await bot.telegram.sendMessage(chatId, text, { ...msgOptions });
+            result = await bot.telegram.sendMessage(chatId, text, options);
     }
+
+    session.recent = session.recent || {};
+    let recentTweets = Object.keys(session.recent);
+    if (recentTweets.length > 100) {
+        recentTweets.sort().slice(0, -100).forEach(k => { delete session.recent[k] });
+    }
+    session.recent[tweet.id_str] = Array.isArray(result) ? result[0].message_id : result.message_id;
 }
 
 function buildTweet(tweet) {
@@ -154,7 +226,9 @@ function buildTweet(tweet) {
         }
 
         if (processedUpTo >= to) {
-            text = text.substring(0, from) + replaceText + text.substring(to);
+            let arr = text.split('');
+            arr.splice(from, to - from, replaceText);
+            text = arr.join('');
             processedUpTo = from;
         }
     }
@@ -168,10 +242,21 @@ function buildTweet(tweet) {
     return {
         mediaType,
         mediaUrls,
-        text: `<a href="https://twitter.com/${ tweet.user.screen_name }/status/${ tweet.id_str }">${ tweet.in_reply_to_status_id_str ? '💬' : '🔗' }</a> | ` +
-              `<a href="https://twitter.com/${ tweet.user.screen_name }">${ user.name }</a>: ${ text }`
+        text: `<a href="https://twitter.com/${ tweet.user.screen_name }/status/${ tweet.id_str }">${ tweet.in_reply_to_status_id_str ? '💬' : '🔗' }${ user.name }</a>: ${ text }`
     }
 }
+
+rl.on('line', async (msg) => {
+    msg = msg.trim();
+    if (msg) {
+        try {
+            await Promise.all(Object.keys(sessions).map(chatId => bot.telegram.sendMessage(chatId, msg, { ...msgOptions })));
+        } catch (e) {
+            console.error(e);
+        }
+        console.log('Sent to all chats.')
+    }
+})
 
 bot.launch();
 srv.listen(80);
